dcfa1cb7c8fad3a1068334dd8f5b2fbe8b196652
Weltraumschaf <ich@weltraumschaf.de>
2011-08-29 23:27:50 +0200

    shorthand app open

---
+++ src/fraise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##
+## Shorthand to open file in Fraise.app
+##
+## See: http://www.macupdate.com/app/mac/33751/fraise
+##
+
+open -a Fraise.app $1
---
+++ src/xcode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+##
+## Short hand to open files in XCode.
+##
+
+open -a XCode $1
