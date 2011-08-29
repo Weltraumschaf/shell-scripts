aaf319e6588c11d4c15084e4d1ac12a4ab46b92e
Weltraumschaf <ich@weltraumschaf.de>
2011-08-29 22:35:18 +0200

    add install script

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+##
+## Installs the scripts in $HOME/bin by symlinking them.
+##
+
 function linkFile {
     source="${PWD}/$1"
-    target="${HOME}/${1/_/.}"
+    targetDir="${HOME}/bin"
+    targetFile="${1/src\//}"
+    targetFile="${targetFile/\.sh/}"
+    target="${targetDir}/${targetFile}"
 
     # Only create backup if target is a file or directory
     if [ -f "${target}" ] || [ -d "${target}" ]; then
+    	echo "Backing up port-remove.sh"
         mv "$target" "$target.bak"
     fi
 
     ln -sf "${source}" "${target}"
 }
 
-for i in _*
+for i in src/*.sh
 do
   linkFile "$i"
 done
