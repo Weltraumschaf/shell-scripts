edf4b9a3fca76a094c22d79992fd4fafab20db3e
Weltraumschaf <ich@weltraumschaf.de>
2011-10-21 13:25:53 +0200

    Add git meta files do ignore.

--- src/dgrep.sh
+++ src/dgrep.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-exec pcregrep -r --exclude='/_darcs|/tmp|\.swp|\.cache|\.o$|\.[tT][tT][fF]$|\.jpg$|\.png$|\.gif$|\.swf$|\.pdf$|\-darcs\-backup' "$@" $PWD
+exec pcregrep -r --exclude='/\.git|/_darcs|/tmp|\.swp|\.cache|\.o$|\.[tT][tT][fF]$|\.jpg$|\.png$|\.gif$|\.swf$|\.pdf$|\-darcs\-backup' "$@" $PWD
