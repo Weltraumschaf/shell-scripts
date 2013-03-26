f2b6d1a61cf2ea3c0bc25784e56c28c1df3a70d2
Weltraumschaf <ich@weltraumschaf.de>
2013-03-26 13:10:52 +0100

    Update brew install script.

---
+++ src/brew-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##
+## Installs a list of my prefered brew tools.
+##
+## Install:
+## ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+##
+## http://mxcl.github.com/homebrew/
+##
+
+brew install \
+    coreutils \
+    aircrack-ng \ 
+    arping \
+    git \
+    htop \
+    jed \
+    john \
+    imagemagick \
+    nmap \
+    pwgen \
+    rhino \
+    scala \
+    svn \
+    unrar \
+    wget \
+    wireshark 
