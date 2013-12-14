066ed0335863af5a85e12c44a45db3086bd669f2
Weltraumschaf <ich@weltraumschaf.de>
2013-12-14 22:51:03 +0100

    Fix git gc script.

--- src/git-gc.sh
+++ src/git-gc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 for dir in $(ls -1 .) ; do
-    if [ "${dir}" != "." && "${dir}" != ".."] ; then
+    if [ "${dir}" != "." ] && [ "${dir}" != ".." ] ; then
         cd "./${dir}"
         echo "Change into ${dir}"
         
         if [ -d "./.git" ] ; then
             echo "Collect garbage..."
-            git gc --aggresive --prune=now
+            git gc --aggressive --prune=now
         else
             echo "Not a git repo. Skipping."
         fi
