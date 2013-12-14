96de76bd8b5909cc06a19e0dceeedf574e6a6d3b
Weltraumschaf <ich@weltraumschaf.de>
2013-12-14 21:50:13 +0100

    Helper to gc git repos.

---
+++ src/git-gc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for dir in $(ls -1 .) ; do
+    if [ "${dir}" != "." && "${dir}" != ".."] ; then
+        cd "./${dir}"
+        echo "Change into ${dir}"
+        
+        if [ -d "./.git" ] ; then
+            echo "Collect garbage..."
+            git gc --aggresive --prune=now
+        else
+            echo "Not a git repo. Skipping."
+        fi
+        
+        cd ..
+    fi
+done
+
+echo "Finished :-)"
