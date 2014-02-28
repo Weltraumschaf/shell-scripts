8232b6323effbe890349d4b6cb693862c05ce54f
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2014-02-28 11:09:57 +0100

    #Add some backup scripts.

---
+++ src/makebackup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+echo "Making backup..."
+
+# %F  full date; same as %Y-%m-%d
+# %H  hour (00..23)
+# %M  minute (00..59)
+# %S  second (00..60)
+now="$(date +%F_%H-%M-%S)"
+prefix="backup_${now}"
+
+backupDir="/media/Backup"
+
+backupFileName="${prefix}.tar"
+tmpDir="${backupDir}/${prefix}"
+
+echo "Creating tmp dir ${tmpDir} ..."
+mkdir -p "${tmpDir}"
+
+echo "Change into directory ${tmpDir} ..."
+cd "${tmpDir}"
+
+sourceDir="/home/Sven.Strittmatter"
+echo "Back up home dir ${sourceDir} ..."
+tar cvpSf "Sven.Strittmatter.tar.bz2" \
+  --one-file-system "${sourceDir}" \
+  --use-compress-program=pbzip2 \
+  --exclude="${sourceDir}/.cache" \
+  --exclude="${sourceDir}/Dropbox" \
+  --exclude="${sourceDir}/.dropbox" \
+  --exclude="${sourceDir}/.Skype" \
+  --exclude="${sourceDir}/.java" \
+  --exclude="*.class" \
+  --exclude="${sourceDir}/.local/share/Trash" \
+  --exclude="${sourceDir}/.m2/repository" \
+  "${sourceDir}"
+
+echo "Backup /etc ..."
+tar cpSjvf etc.tar.bz2 /etc
+
+echo "Backing up installed packages ..."
+# To restore
+# sudo dpkg --set-selections < /tmp/dpkglist.txt
+# sudo apt-get -y update
+# sudo apt-get dselect-upgrade
+dpkg --get-selections > dpkglist.txt
+bzip2 dpkglist.txt
+
+echo "Create final archive ${backupDir}/${backupFileName} ..."
+tar cvf "${backupDir}/${backupFileName}" "${tmpDir}"
+
+echo "Removing tmp dir ${tmpDir} ..."
+rm -rvf "${tmpDir}"
+
+echo "Done."
---
+++ src/timemachine.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# http://blog.interlinked.org/tutorials/rsync_time_machine.html
+
+set -e
+
+baseDir="/media/Backup/Timemachine"
+
+if [ ! -d "${baseDir}" ] ; then
+  echo "Target dir '${baseDir}' does not exists!"
+  exit 1
+fi
+
+date=`date "+%Y-%m-%dT%H:%M:%S"`
+current="${baseDir}/current"
+backup="${baseDir}/${date}"
+
+rsync -av --delete --one-file-system --partial \
+  --exclude='.cache' \
+  --exclude='Dropbox' \
+  --exclude='.dropbox' \
+  --exclude='.Skype' \
+  --exclude='.java' \
+  --exclude='*.class' \
+  --exclude='.local/share/Trash' \
+  --exclude='.m2/repository' \
+  --link-dest="${current}" "${HOME}" "${backup}"
+
+rm -fv "${current}"
+ln -s "${backup}" "${current}"
+
+echo "Done."
