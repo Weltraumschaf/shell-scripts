332d1aaef656bb340e15fc6a8fa83000d4905363
Sven Strittmatter <ich@weltraumschaf.de>
2014-03-11 20:16:36 +0100

    Script to checkout al branches.

---
+++ src/git-checkout-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/67699/how-do-i-clone-all-remote-branches-with-git
+
+for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
+    git branch --track ${branch##*/} $branch
+done
+
+git fetch --all
+git pull --all
