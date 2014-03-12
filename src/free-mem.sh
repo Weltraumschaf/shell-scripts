c2085c3918c8894c2772ef1aca75c1262de88ef5
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2014-03-12 10:14:37 +0100

    Remove h switch.

--- src/free-mem.sh
+++ src/free-mem.sh
@@ -2,11 +2,11 @@
 
 # http://www.upubuntu.com/2011/09/how-to-free-up-ram-on-ubuntudebian.html
 
-free -mh
+free -m
 
 echo "Free RAM..."
 sync
 su
 echo 3 > /proc/sys/vm/drop_caches
 
-free -mh
+free -m
