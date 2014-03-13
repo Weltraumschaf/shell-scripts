fb8fc1a12e9aed3b4fb37b691aa5bd2de8594817
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2014-03-13 11:18:23 +0100

    Add script to crypt files

---
+++ src/encrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# http://sleepyhead.de/howto/?href=crypt
+
+if [ "${1}" == "" ]; then
+    echo "No input file given!"
+    exit 1
+fi
+
+IN_FILE="${1}"
+
+echo "Encrypting file ${IN_FILE} ..."
+openssl aes-256-cbc -salt -in "${IN_FILE}" -out "${IN_FILE}.aes"
