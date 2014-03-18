b4f57d37b16b11b8b3f16b680d00cb4bb941a641
Sven Strittmatter <ich@weltraumschaf.de>
2014-03-18 21:45:21 +0100

    List 30 largest files.

--- src/git-find-large.sh
+++ src/git-find-large.sh
@@ -12,8 +12,8 @@
 # set the internal field spereator to line break, so that we can iterate easily over the verify-pack output
 IFS=$'\n';
 
-# list all objects including their size, sort by size, take top 10
-objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head`
+# list all objects including their size, sort by size, take top 30
+objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head -n 30`
 
 echo "All sizes are in kB's. The pack column is the size of the object, compressed, inside the pack file."
 
