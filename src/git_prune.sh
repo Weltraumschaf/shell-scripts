6e1f83bb27253d3651a50ea456d76e81900adc5d
Sven Strittmatter <ich@weltraumschaf.de>
2014-03-18 21:48:16 +0100

    Add prune script.

---
+++ src/git_prune.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git reset --hard
+git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
+git reflog expire --expire=now --all
+git gc --aggressive --prune=now
