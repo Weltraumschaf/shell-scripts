b73e24437bc5c7487cced8cc89938090be499e3b
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2014-04-30 15:11:56 +0200

    Add script to remove old kernels.

---
+++ src/remove-old-kernels.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# http://askubuntu.com/questions/2793/how-do-i-remove-or-hide-old-kernel-versions-to-clean-up-the-boot-menu-or-free-sp
+#
+
+sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
