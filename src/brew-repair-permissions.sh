f9daa11c253d314efd0f86ad3aafbe6d8babe196
Sven Strittmatter <ich@weltraumschaf.de>
2014-06-28 23:25:22 +0200

    Fix script.

--- src/brew-repair-permissions.sh
+++ src/brew-repair-permissions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo chmod -vRh a+rX /usr/local/Cellar
+sudo chmod -vR a+rX /usr/local/Cellar
+sudo chmod -vh a+rX /usr/local/Cellar
 sudo chmod -vh a+rx /usr/local/bin/*
-sudo chmod -vh a+rx /usr/local/Cellar/maven/3.2.1/bin/*
+sudo chmod -vh a+rx /usr/local/Cellar/maven/3.2.2/bin/*
 
 echo "Finished :)"
