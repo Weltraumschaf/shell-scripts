43d96e7a19976ee522eb05f4b67e53fa486b85fe
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2014-07-03 15:37:43 +0200

    Script for backing up installed packages.

---
+++ src/backup-package-selections.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# To restore
+# sudo dpkg --set-selections < /tmp/dpkglist.txt
+# sudo apt-get -y update
+# sudo apt-get dselect-upgrade
+
+# %F  full date; same as %Y-%m-%d
+# %H  hour (00..23)
+# %M  minute (00..59)
+# %S  second (00..60)
+now="$(date +%F_%H-%M-%S)"
+filename="${now}_dpkglist.txt"
+
+echo "Backing up installed packages ..."
+
+dpkg --get-selections > "${filename}"
+bzip2 "${filename}"
