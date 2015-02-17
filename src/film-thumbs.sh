7b1519d41e9d0663f669bfc817ac92098b2c7b69
Sven Strittmatter <ich@weltraumschaf.de>
2015-02-17 17:08:09 +0100

    Script to generate thumbs from movies.

---
+++ src/film-thumbs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dir="${1}"
+cwd=$(pwd)
+#vlc="/Applications/VLC.app/Contents/MacOS/VLC"
+vlc="/opt/homebrew-cask/Caskroom/vlc/2.1.5/VLC.app/Contents/MacOS/VLC"
+
+echo "Make thumbs of movies in ${dir} ..."
+
+find "${dir}" -type f | egrep -i "\.(avi|flv|mov|mp4|mpeg|mpg|wmv)$" | while read file; do
+    echo "Thumbing ${file} ..."
+    # https://wiki.videolan.org/How_to_create_thumbnails/
+    thumbdir="${cwd}${file%.*}"
+    mkdir -p "${thumbdir}"
+    "${vlc}" \
+        --rate=1 \
+        --video-filter=scene \
+        --vout=dummy \
+        --aout=dummy \
+        --start-time=100 \
+        --stop-time=101 \
+        --scene-format=png \
+        --scene-ratio=24 \
+        --scene-prefix=snap \
+        --scene-path="${thumbdir}" \
+        --play-and-exit \
+        "${file}" vlc://quit &
+        sleep 2
+        pkill vlc
+        exit
+done
