b951aead7b2b606d30d886451dba64661a8db939
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-12-11 15:44:54 +0100

    Do not remove target and manifest.

--- src/del-eclipse-shit.sh
+++ src/del-eclipse-shit.sh
@@ -4,8 +4,6 @@ CWD=`pwd`
 
 echo "Cleaning ${CWD} from eclipse crap files..."
 
-find "${CWD}" -type d -name "target" | xargs rm -rfv
 find "${CWD}" -type d -name "\.settings" | xargs rm -rfv
 find "${CWD}" -type f -name "\.project" | xargs rm -v
 find "${CWD}" -type f -name "\.classpath" | xargs rm -v
-find "${CWD}" -type f -name "MANIFEST.MF" | xargs rm -v
