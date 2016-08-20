7fb1c0c0595f423238f1d715cd55aa14ec218e21
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-08-20 22:51:24 +0200

    Verify input parameter.

--- src/verify_johan_backup.sh
+++ src/verify_johan_backup.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env sh
 
+BACKUP_TO_VERIFY="${1}"
+
+if [ "${BACKUP_TO_VERIFY}" == "" ] ; then
+    echo "No backup given to verify!"
+    exit 1
+fi
+
+if [ ! -e "${BACKUP_TO_VERIFY}" ] ; then
+    echo "File does not exist ${BACKUP_TO_VERIFY}!"
+    exit 2
+fi
+
 CWD=$(pwd)
 
 cd /storage/backups/johan.weltraumschaf.de
-tar xvf "${1}"
+tar xvf 
 
 cd backup/
 find . -name "*.bz2" | xargs pbunzip2 -v
