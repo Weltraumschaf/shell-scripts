e73984ec7b0d53932631a9c9e7092ca1f3afbddd
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-06-06 13:07:54 +0200

    Better brew perm fix script.

--- src/brew-repair-permissions.sh
+++ src/brew-repair-permissions.sh
@@ -1,6 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-sudo chmod -vR a+rX /usr/local
-sudo chmod -vh a+rX /usr/local/bin/*
+# https://gist.github.com/jaibeee/9a4ea6aa9d428bc77925
+
+# Configure homebrew permissions to allow multiple users on MAC OSX.
+# Any user from the admin group will be able to manage the homebrew and cask installation on the machine.
+
+# allow admins to manage homebrew's local install directory
+chgrp -R admin /usr/local
+chmod -R g+w /usr/local
+
+# allow admins to homebrew's local cache of formulae and source files
+chgrp -R admin /Library/Caches/Homebrew
+chmod -R g+w /Library/Caches/Homebrew
+
+# if you are using cask then allow admins to manager cask install too
+chgrp -R admin /opt/homebrew-cask
+chmod -R g+w /opt/homebrew-cask
 
 echo "Finished :)"
