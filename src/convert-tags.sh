8e69cc369b1a58c38649c29a5587ed088bbbf4ca
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-12-05 12:54:18 +0100

    More debug output

--- src/convert-tags.sh
+++ src/convert-tags.sh
@@ -17,20 +17,21 @@ set -e
 # Since tags in svn are real branches, create git tags from tag branches.
 git for-each-ref --format="%(refname:short) %(objectname)" refs/remotes/origin/tags | cut -d / -f 3- | while read ref
 do
-    echo "Import tag for $ref"
+    echo "Import tag for $ref ..."
     git tag -a $ref -m 'import tag from svn'
 done
 
 # Delete tag branches.
 git for-each-ref --format="%(refname:short)" refs/remotes/origin/tags | cut -d / -f 1- | while read ref
 do 
-    echo "Delete tag branch $ref"
+    echo "Delete tag branch $ref ..."
     git branch -rd $ref
 done
 
 # Since tags marked in the previous step point to a commit "create tag", we need to derive "real" tags, i.e. parents of "create tag" commits.
 git for-each-ref --format="%(refname:short)" refs/tags | while read ref
 do
+    echo "Derive \"real\" tags from \"create tag\" commits ..."
   tag=`echo $ref | sed 's/_/./g'` # give tags a new name
   echo $ref -\> $tag
   git tag -a $tag `git rev-list -2 $ref | tail -1` -m "proper svn tag"
