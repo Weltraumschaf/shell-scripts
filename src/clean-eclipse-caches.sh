126b3e9fa956410f4245021853c6a0b952f35f56
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-04-25 13:07:07 +0200

    Add script to clean Eclipse caches

---
+++ src/clean-eclipse-caches.sh
@@ -0,0 +1,8 @@
+#/usr/bin/env bash
+
+set -e
+set -u
+
+rm -rfv "/cygdrive/c/TACO-IDE/workspaces/default-workspace/.metadata/.plugins/org.eclipse.pde.core/.bundle_pool"
+rm -rfv "/cygdrive/c/Documents and Settings/sst/.p2"
+rm -rfv "/home/sst/.m2/repository"
