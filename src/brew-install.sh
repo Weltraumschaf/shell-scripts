035feb14828f197cabfb32ca81ce6b27721c017d
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-10-10 11:17:14 +0200

    Add some casks

--- src/brew-install.sh
+++ src/brew-install.sh
@@ -10,9 +10,14 @@
 ##
 
 formulas="ant coreutils aircrack-ng arping git htop jed john imagemagick nmap maven pwgen rhino scala svn tree tmux unrar wget"
+casks="vlc iterm2"
 
-for formula in $formulas ; do
-    brew install $formula
+for formula in $formulas; do
+    brew install "${formula}"
+done
+
+for cask in $casks; do
+    brew cask install "${cask}"
 done
 
 brew install wireshark --with-qt
