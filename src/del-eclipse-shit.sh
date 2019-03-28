9bfd3ac727be61bf076707b2101a910a02c8b7e5
Sven Strittmatter <sven.strittmatter@iteratec.de>
2019-03-28 11:07:48 +0100

    Abort on errors and undefined vars

--- src/del-eclipse-shit.sh
+++ src/del-eclipse-shit.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-CWD=`pwd`
+set -eu
+CWD=$(pwd)
 
 echo "Cleaning ${CWD} from eclipse crap files..."
 
-find "${CWD}" -type d -name "\.settings" | xargs rm -rfv
+find "${CWD}" -type d -name "\.settings"  | xargs rm -rfv
 find "${CWD}" -type f -name "\.project" | xargs rm -v
-find "${CWD}" -type f -name "\.classpath" | xargs rm -v
+find "${CWD}" -type f -name "\.classpath"  | xargs rm -v
