3ec5081ce3e0c155ecb26d99eeac6a439dd72fd8
Sven Strittmatter <ich@weltraumschaf.de>
2020-01-06 12:10:24 +0100

    Add help
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/backup-alpha.sh
+++ src/backup-alpha.sh
@@ -7,16 +7,16 @@ set -u
 # Backscritp for my FreeNAA baes on https://www.ixsystems.com/community/threads/zfs-send-to-external-backup-drive.17850/
 #
 
-ALLOWED_CMDS="init|backup"
-USAGE="$(basename "$0") ALLOWED_CMDS <source> <target>"
+ALLOWED_COMMANDS="init|backup|help"
+USAGE="$(basename "$0") ${ALLOWED_COMMANDS} <source-pool> <target-pool>"
 
-init() {
+initialize_backup() {
     echo "init"
     # zfs snapshot -r zstorage@backup
     # zfs send -R zstorage@backup | zfs receive -vF backup
 }
 
-backup() {
+incremental_backup() {
     echo "backup"
     # zfs rename -r zstorage@backup zstorage@previous_backup
     # zfs snapshot zstorage@backup
@@ -24,19 +24,45 @@ backup() {
     # zfs destroy -r zstorage@previous_backup
 }
 
-CMD="${1:-}"
+show_usage() {
+    echo "Usage: ${USAGE}"
+}
 
-case "${CMD}" in
-    init) init ;;
-    backup) backup ;;
+show_help () {
+    echo "This script backups a ZFS pool via send and receive to a target pool."
+    echo
+    show_usage
+    echo
+    echo "  init      Does the initial backup. This may take a while."
+    echo "  backup    Does an incremental backup."
+    echo "  help      Show this help."
+    echo
+}
+
+COMMAND="${1:-}"
+SOURCE_POOL="${2:-}"
+TARGET_POOL="${3:-}"
+
+case "${COMMAND}" in
+    init)
+        initialize_backup "${SOURCE_POOL}" "${TARGET_POOL}"
+        ;;
+    backup)
+        incremental_backup "${SOURCE_POOL}" "${TARGET_POOL}"
+        ;;
+    help)
+        show_help
+        ;;
     *)
-        if [ "" = "${CMD}" ]; then
-            echo "No command given! Use on of: ${ALLOWED_CMDS}" | sed -e 's/|/, /g'
-            echo "${USAGE}"
+        if [ "" = "${COMMAND}" ]; then
+            echo "Error: No command given! Use on of: ${ALLOWED_COMMANDS}" | sed -e 's/|/, /g'
+            show_usage
+            echo
             exit 1
         else
-            echo "Unnown command '${CMD}' given! Use on of: ${ALLOWED_CMDS}" | sed -e 's/|/, /g'
-            echo "${USAGE}"
+            echo "Error: Unnown command '${COMMAND}' given! Use on of: ${ALLOWED_COMMANDS}" | sed -e 's/|/, /g'
+            show_usage
+            echo
             exit 2
         fi
 esac
