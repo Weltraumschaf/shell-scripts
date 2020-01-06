f74e035d52f15bea9c2d05ff528bd1f5f73f86d6
Sven Strittmatter <ich@weltraumschaf.de>
2020-01-06 11:58:53 +0100

    Add some parameters and error handling
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/backup-alpha.sh
+++ src/backup-alpha.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
 set -u
@@ -7,14 +7,36 @@ set -u
 # Backscritp for my FreeNAA baes on https://www.ixsystems.com/community/threads/zfs-send-to-external-backup-drive.17850/
 #
 
+ALLOWED_CMDS="init|backup"
+USAGE="$(basename "$0") ALLOWED_CMDS <source> <target>"
+
+init() {
+    echo "init"
+    # zfs snapshot -r zstorage@backup
+    # zfs send -R zstorage@backup | zfs receive -vF backup
+}
+
+backup() {
+    echo "backup"
+    # zfs rename -r zstorage@backup zstorage@previous_backup
+    # zfs snapshot zstorage@backup
+    # zfs send -Ri zstorage@previous_backup zstorage@backup | zfs receive -v backup
+    # zfs destroy -r zstorage@previous_backup
+}
+
 CMD="${1:-}"
 
-if [ "${CMD}" = "init" ]; then
-    zfs snapshot -r zstorage@backup
-    zfs send -R zstorage@backup | zfs receive -vF backup
-else
-    zfs rename -r zstorage@backup zstorage@previous_backup
-    zfs snapshot zstorage@backup
-    zfs send -Ri zstorage@previous_backup zstorage@backup | zfs receive -v backup
-    zfs destroy -r zstorage@previous_backup
-fi
+case "${CMD}" in
+    init) init ;;
+    backup) backup ;;
+    *)
+        if [ "" = "${CMD}" ]; then
+            echo "No command given! Use on of: ${ALLOWED_CMDS}" | sed -e 's/|/, /g'
+            echo "${USAGE}"
+            exit 1
+        else
+            echo "Unnown command '${CMD}' given! Use on of: ${ALLOWED_CMDS}" | sed -e 's/|/, /g'
+            echo "${USAGE}"
+            exit 2
+        fi
+esac
