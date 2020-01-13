159be7364fd03a9a0ba099d647b41ec707a6d469
Sven Strittmatter <ich@weltraumschaf.de>
2020-01-13 22:02:37 +0100

    Script to clone all my repos
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

---
+++ src/github-clone-all-repos.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+#
+# Clone all my GitHub repos at current working direcotry.
+#
+# Based on https://gist.github.com/milanboers/f34cdfc3e1ad9ba02ee8e44dae8e093f
+#
+
+# | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
+
+curl -s https://api.github.com/users/Weltraumschaf/repos?per_page=200 \
+    | jq -r ".[].git_url" \
+    |  xargs -L1 -P 4 git clone
