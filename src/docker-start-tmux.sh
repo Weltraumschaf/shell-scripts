91ff1d3e81b16a16126e0cb0240d4dd9b291afbb
Sven Strittmatter <ich@weltraumschaf.de>
2020-01-25 21:57:35 +0100

    Fix the logfile name
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/docker-start-tmux.sh
+++ src/docker-start-tmux.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 #
+# Script to startr all my containers in a tmux session.
+#
 # tmux attach-session -t docker-session
 # tmux kill-session -t docker-session
+#
 
 tmux new-session -d -s docker-session
 
-tmux send 'cd /root/docker/locatord && ./start.sh | tee wohnungssuche.log' ENTER;
+tmux send 'cd /root/docker/locatord && ./start.sh | tee locatord.log' ENTER;
 tmux split-window;
 tmux send 'cd /root/docker/wohnungssuche && ./start.sh | tee wohnungssuche.log' ENTER;
 
