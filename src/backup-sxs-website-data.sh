c2573b798f021fc2d8307568e9c1a4028ef0fec3
Sven Strittmatter <ich@weltraumschaf.de>
2020-07-15 22:20:27 +0200

    Use right user
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/backup-sxs-website-data.sh
+++ src/backup-sxs-website-data.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-rsync -avzh -e 'ssh -p 4222' root@johan.weltraumschaf.de:/var/svn /mnt/zstorage/backups/legacy
+rsync -avzh -e 'ssh -p 4222' sxs@johan.weltraumschaf.de:/var/svn /mnt/zstorage/backups/legacy
 chown -R sxs:sxs /mnt/zstorage/backups/legacy
