ce2db9f01e6deb9ab724a4bb956eca838e27a6d7
Sven Strittmatter <ich@weltraumschaf.de>
2020-07-19 19:35:19 +0200

    Swith to the new legacy machine
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/backup-svn-website-data.sh
+++ src/backup-svn-website-data.sh
@@ -5,7 +5,7 @@ set -eu
 base_dir="/mnt/zstorage/backups/legacy/"
 
 rsync -avzh --no-perms --delete -e 'ssh -p 4222' \
-    sxs@johan.weltraumschaf.de:/var/svn \
+    wesley@tpol.weltraumschaf.de:/var/svn \
     "${base_dir}"
 
 chown -R sxs:sxs "${base_dir}/svn"
--- src/backup-sxs-website-data.sh
+++ src/backup-sxs-website-data.sh
@@ -5,7 +5,7 @@ set -eu
 base_dir="/mnt/zstorage/backups/legacy/"
 
 rsync -avzh --no-perms --delete -e 'ssh -p 4222' \
-    sxs@johan.weltraumschaf.de:/home/sxs/public_html \
+    wesley@tpol.weltraumschaf.de:/var/www/sxs.weltraumschaf.de \
     "${base_dir}"
 
 chown -R sxs:sxs "${base_dir}/public_html"
