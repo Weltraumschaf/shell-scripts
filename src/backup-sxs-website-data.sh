b3f5a5ee435db72830367645319b438adee6a250
Sven Strittmatter <ich@weltraumschaf.de>
2020-07-20 12:43:09 +0200

    Fix the target dir
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- src/backup-sxs-website-data.sh
+++ src/backup-sxs-website-data.sh
@@ -8,4 +8,4 @@ rsync -avzh --no-perms --delete -e 'ssh -p 4222' \
     wesley@tpol.weltraumschaf.de:/var/www/sxs.weltraumschaf.de \
     "${base_dir}"
 
-chown -R sxs:sxs "${base_dir}/public_html"
+chown -R sxs:sxs "${base_dir}/sxs.weltraumschaf.de"
