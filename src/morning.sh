f2baf324c0f018cf49e053ca436f5f280b5e4a73
Sven Strittmatter <ich@weltraumschaf.de>
2021-08-31 09:26:37 +0200

    Script to start the morning
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

---
+++ src/morning.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+#
+# Morning script to start a work day
+#
+
+set -euo pipefail
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    echo "This script is only for macOS!"
+    exit 1
+fi
+
+declare -a apps=(
+    "BusyCal.app"
+    "Discord.app"
+    "Fork.app"
+    "Joplin.app"
+    "Mail.app"
+    "Microsoft Teams.app"
+    "MultiFirefox.app"
+    "NetNewsWire.app"
+    "Slack.app"
+    "TextMate.app"
+    "Thunderbird.app"
+    "Trello.app"
+    "iTerm.app"
+)
+
+cat <<- "EOT"
+
+    ____                 _                              _             _
+   / ___| ___   ___   __| |  _ __ ___   ___  _ __ _ __ (_)_ __   __ _| |
+  | |  _ / _ \ / _ \ / _` | | '_ ` _ \ / _ \| '__| '_ \| | '_ \ / _` | |
+  | |_| | (_) | (_) | (_| | | | | | | | (_) | |  | | | | | | | | (_| |_|
+   \____|\___/ \___/ \__,_| |_| |_| |_|\___/|_|  |_| |_|_|_| |_|\__, (_)
+                                                                |___/
+
+EOT
+
+echo "Starting some apps..."
+
+for app in "${apps[@]}"; do
+    open -a "${app}"
+done
+
+echo "Everything startet!"
+
+cat <<- "EOT"
+
+    Morning Todos
+    -------------
+
+    * topgrade
+    * Jira
+    * DevOps Team
+    * SECKT
+    * GitHub
+    * Notifications
+    * Projectboard
+    * Trello
+    * SCB Retromaßnahmen
+
+EOT
+
+ultralist list
